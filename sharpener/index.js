da51958f80bd0691f0fb0e93bc08c3f97fe5bb41
NSGahlot <ngahlot411@gmail.com>
2024-01-17 23:56:55 +0530

    first commit

---
+++ sharpener/index.js
@@ -0,0 +1,10 @@
+// Write your code below:
+document.getElementById("main-heading").textContent = "Fruit World";
+document.getElementById("main-heading").style.color = "orange";
+document.getElementById("header").style.backgroundColor = "green";
+document.getElementById("header").style.borderBottomColor = "orange";
+document.getElementById("basket-heading").style.color = "green";
+// const para = document.createElement("p");
+// para.textContent = "Please visit us again";
+// document.getElementById("thanks").appendChild(para);
+document.getElementById("thanks").innerHTML = `<p>Please visit us again</p>`;
